c29856fa00cb7af9ba92d552d971cf06a3176138
puckey <jonathan@puckey.studio>
2020-05-27 11:39:13 +0200

    return undefined for android

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
  * @providesModule react-native-icloudstore
  */
 
-import { AsyncStorage, NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-const iCloudStorage = Platform.OS === 'ios' ? NativeModules.RNICloudStorage : AsyncStorage;
-
-export default iCloudStorage;
-
+export const iCloudStorage = Platform.OS === 'ios' ? NativeModules.RNICloudStorage : undefined;
